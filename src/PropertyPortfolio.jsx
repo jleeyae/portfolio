fb4c43cae84b7cb3b3dde4a61459f5a2557ed73d
jleeyae <65741088+jleeyae@users.noreply.github.com>
2025-12-22 14:44:49 -0700

    Add files via upload

---
+++ src/PropertyPortfolio.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from 'react'
+import { Download, Home, TrendingUp, Heart, MapPin, DollarSign, Bed, Bath, Maximize } from 'lucide-react'
+
+const PropertyPortfolio = () => {
+  // all the state, groups array, JSX, etc. from your file
+}
+
+export default PropertyPortfolio
